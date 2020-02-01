afa0a388f05862bf5a1f1bfd7e365457996047ff
Craig Crossley <ccrossley@Nicoles-MacBook-Pro.local>
2020-02-01 12:42:14 -0800

    added station subs and made well inherit from station

---
+++ Assets/Scripts/Stations/Well.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Well : Station
+{
+    public bool isCurrentlyInteractable => true;
+
+    public override void Interact(PlayerController player)
+    {
+        if (player.heldItem is ItemWatteringCan can)
+        {
+            can.Fill();
+            can.StartAnimation();
+        }
+    }
+}
