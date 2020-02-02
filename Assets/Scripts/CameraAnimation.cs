6da0912bd8a69ea4dde038801ee96e9a058bec5a
bpFarrell <brandon@bpfarrell.com>
2020-02-02 12:30:08 -0800

    CameraAnimation, and prefabbed seed boxes

---
+++ Assets/Scripts/CameraAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAnimation : MonoBehaviour
+{
+    public Animation anim;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim["CameraPivot"].wrapMode = WrapMode.Once;
+
+        anim.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
