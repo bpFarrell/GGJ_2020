6043c89726eeea3caeef08ad11c034850229fb5a
Craig Crossley <ccrossley@Nicoles-MacBook-Pro.local>
2020-02-02 13:02:59 -0800

    seed colors...yup

--- Assets/Scripts/Items/ItemSeedBag.cs
+++ Assets/Scripts/Items/ItemSeedBag.cs
@@ -1,8 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemSeedBag : ItemBase
 {
     public PlantType plantType;
+    private void Start()
+    {
+        var meshRenderers = GetComponentsInChildren<MeshRenderer>();
+        
+        //they've all got the same material so just use the first one to make the new ref
+        var plantColor = Color.red;
+
+        if (plantType == PlantType.Green)
+        {
+            plantColor = Color.green;
+        }
+        else if (plantType == PlantType.Yellow)
+        {
+            plantColor = Color.yellow;
+        }
+
+        var newMaterial = new Material(meshRenderers[0].material)
+        {
+            color = plantColor
+        };
+
+        foreach (var meshRenderer in meshRenderers)
+        {
+            meshRenderer.material = newMaterial;
+        }
+    }
+    
+    
 }
