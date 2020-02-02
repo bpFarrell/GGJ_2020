9b2d0f9b1846831ea6ee083537fb281109f02232
JasonCrossley <rojibora@gmail.com>
2020-02-02 00:02:58 -0800

    Fix: Removed unused vehicle assets Add: Car spawn manager logic.

---
+++ Assets/Scripts/CarManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class CarManager : MonoBehaviour {
+    public CarBehavior prefab;
+
+    public CarBehavior[] carList = {null, null, null};
+    public Vector3[] positionList = new Vector3[3];
+
+    public AnimationCurve chanceCurve;
+
+    public bool spawn;
+
+    private float counter;
+    private void Update() {
+        if (spawn) {
+            spawn = false;
+            SpawnCar(RandomDropBox());
+        }
+    }
+
+    private PlantType[] RandomDropBox() {
+        int rand = Random.Range(1, 5);
+        rand = (int)(chanceCurve.Evaluate(rand / 4f)*4);
+        PlantType[] typeArr = new PlantType[rand];
+        
+        for (var i = 0; i < typeArr.Length; i++) {
+            typeArr[i] = RandomType();
+        }
+
+        return typeArr;
+    }
+    
+    private PlantType RandomType() {
+        int rand = Random.Range(0, 3);
+        switch (rand) {
+            case 0:
+                return PlantType.Red;
+            case 1:
+                return PlantType.Green;
+            case 2:
+                return PlantType.Yellow;
+            default:
+                return PlantType.Yellow;
+        }
+    }
+
+    private void SpawnCar(PlantType[] plantTypes) {
+        if (CountNullCars() == 0) return;
+        var go = Instantiate(prefab, transform);
+        var nextAvailableIndex = NextAvailableIndex();
+        carList[nextAvailableIndex] = go;
+        go.target = positionList[nextAvailableIndex];
+        go.dropBox.InitDelivery(plantTypes);
+    }
+
+    private int CountNullCars() {
+        return carList.Where((c) => { return c == null; }).Count();
+    }
+    private int NextAvailableIndex() {
+        for (var i = 0; i < carList.Length; i++) {
+            if (carList[i] == null) return i;
+        }
+        return -1;
+    }
+}
